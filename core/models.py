3edf7dda382a9eea37ef6e5d8f4e3c06ea98f951
Renan Santana <renan_ota@hotmail.com>
2022-05-31 00:50:08 -0300

    add str on models

--- core/models.py
+++ core/models.py
@@ -8,3 +8,6 @@ class Message(models.Model):
     date = models.DateField(auto_now=True)
     author = models.CharField(max_length=255)
     text = models.TextField()
+
+    def __str__(self) -> str:
+        return self.author
