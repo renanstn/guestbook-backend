0cd51ed9a20785cb685e69b079afaffd47358443
Renan Santana <renan_ota@hotmail.com>
2022-05-31 01:08:59 -0300

    set permissions

--- guestbook/settings.py
+++ guestbook/settings.py
@@ -102,7 +102,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+        # "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+        "rest_framework.permissions.AllowAny"
     ]
 }
 
